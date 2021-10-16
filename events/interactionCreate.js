5ff256b3d1bef951c846a54690b26f022afce508
Snofu12 <haihaihellohi@gmail.com>
2021-10-16 03:43:16 -0400

    fixed event handlers

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,15 @@
 module.exports = {
     name: 'interactionCreate',
-    execute(interaction){
-        console.log(`${interation.user.tag} in #${interaction.channel.name} triggerd an interaction.`);
+    async execute(interaction) {
+        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) return;
+
+        try{
+            await command.execute(interaction);
+        } catch(error) {
+            console.error(error);
+            return interaction.reply({ content: 'There was an error while executing this command!', ephermeral: true});
+        }
     },
 };
